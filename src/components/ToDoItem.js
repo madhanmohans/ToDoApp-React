419fc01c34e16d42c8d5eef2b11f4d6391ee68d3
madhanmohans <sherlockholmes.missme@gmail.com>
2024-05-23 15:28:17 +0530

    able to mark as completed (strike through text)

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,5 +1,17 @@
+import { useState } from "react"
+
 export default function ToDoItem({item}) {
+    const [isItemDone, setItemDone] = useState(false);
+
+    function handleClick() {
+        setItemDone(!isItemDone);
+    }
+
     return (
-        <li>{item}</li>
+        <li onClick={ handleClick } style={ { cursor: 'pointer'}}>
+            {
+                isItemDone? <s> {item} </s> : item
+            }
+        </li>
     )
 }
